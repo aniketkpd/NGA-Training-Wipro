764005d4282ac83e04bdf9f801e674da4fa21670
Aniket <aniketsingh7855@gmail.com>
2026-04-20 12:06:33 +0530

    day5

--- Day5/PatternMatching.cs
+++ Day5/PatternMatching.cs
@@ -6,58 +6,79 @@
 //Step 4: Showing output of pattern matching with properties to check if an object has certain
 
 
-
-
-public class PatternMatching
+namespace day5
 {
-    public static void Main()
+
+
+    public class PatternMatching
     {
-
-
-
-        //object obj = "Hello, World!";
-        //if(obj is string str)
-        //{
-        //    Console.WriteLine($"The string is: {str}");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("The object is not a string.");
-        //}
-
-
-
-
-
-
-
-
-
-
-        Console.WriteLine("Enter a value:");
-        string? input = Console.ReadLine();
-
-        object? value = int.TryParse(input, out int i) ? i :
-                       double.TryParse(input, out double d) ? d :
-                       input;
-
-        switch (value)
+        public static void Main()
         {
-            case int x:
+
+
+            //Use of is in pattern matching - 1
+            //object obj = "Hello, World!";
+            //if(obj is string str)
+            //{
+            //    Console.WriteLine($"The string is: {str}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The object is not a string.");
+            //}
+
+
+
+
+
+
+
+
+
+            //Use of is pattern matching - 2
+            Console.WriteLine("Enter a value:");
+            string? input = Console.ReadLine();
+
+
+
+            object? value = int.TryParse(input, out int i) ? i :
+                           double.TryParse(input, out double d) ? d :
+                           input;
+
+            //Using switch
+            //switch (value)
+            //{
+            //    case int x:
+            //        Console.WriteLine($"The input is an integer: {x}");
+            //        break;
+
+            //    case double y:
+            //        Console.WriteLine($"The input is a double: {y}");
+            //        break;
+
+            //    case string s:
+            //        Console.WriteLine($"The input is a string: {s}");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("The input is of an unknown type.");
+            //        break;
+            //}
+
+
+            //Using if-else
+            if (value is int x)
+            {
                 Console.WriteLine($"The input is an integer: {x}");
-                break;
-
-            case double y:
+            }
+            else if (value is double y)
+            {
                 Console.WriteLine($"The input is a double: {y}");
-                break;
-
-            case string s:
+            }
+            else if (value is string s)
+            {
                 Console.WriteLine($"The input is a string: {s}");
-                break;
-
-            default:
-                Console.WriteLine("The input is of an unknown type.");
-                break;
+            }
         }
     }
 }
