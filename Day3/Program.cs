e80b4f1bcd99f74b6981ee87d72b5b02cfc603dd
Aniket <aniketsingh7855@gmail.com>
2026-04-23 17:26:30 +0530

    day8

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,31 +1,29 @@
 ﻿
 //Hello World program
-using System;
+//using System;
 
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Hello World");
-    }
-}
-
-
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Hello World");
+//    }
+//}
 
 
 
 
-//Boxing unboxing Concept
-Console.WriteLine("Boxing and Unboxing in C#");
-//value type variables 
-int a = 10;
-//Boxing
-object obj = a;
-//Unboxing
-int v = (int)obj;
-Console.WriteLine(obj);
 
 
+////Boxing unboxing Concept
+//Console.WriteLine("Boxing and Unboxing in C#");
+////value type variables 
+//int a = 10;
+////Boxing
+//object obj = a;
+////Unboxing
+//int v = (int)obj;
+//Console.WriteLine(obj);
 
 
 
@@ -40,103 +38,105 @@ Console.WriteLine(obj);
 
 
 
-//classes and objects
-class Car
-{
-    //feild
-    string color = "red";
-
-    static void Main(string[] args)
-    {
-        //making object
-        Car myObj = new Car();
-        Console.WriteLine(myObj.color);
-    }
-}
 
 
+////classes and objects
+//class Car
+//{
+//    //feild
+//    string color = "red";
 
-
-using System;
-
-class Animal
-{
-    //feild
-    public string name;
-
-    //method
-    public void MakeSound()
-    {
-        Console.WriteLine(name + " makes sound");
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        Animal a1 = new Animal();
-        a1.name = "Dog";
-
-        //accessing members
-        a1.MakeSound();
-        Console.WriteLine(a1.name);
-    }
-}
+//    static void Main(string[] args)
+//    {
+//        //making object
+//        Car myObj = new Car();
+//        Console.WriteLine(myObj.color);
+//    }
+//}
 
 
 
 
+//using System;
+
+//class Animal
+//{
+//    //feild
+//    public string name;
+
+//    //method
+//    public void MakeSound()
+//    {
+//        Console.WriteLine(name + " makes sound");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Animal a1 = new Animal();
+//        a1.name = "Dog";
+
+//        //accessing members
+//        a1.MakeSound();
+//        Console.WriteLine(a1.name);
+//    }
+//}
 
 
 
 
-using System;
 
-class Student
-{
-    public string Name;
-    public int Id;
 
-    public Student(string name, int id)
-    {
-        this.Name = name;
-        this.Id = id;
-    }
 
-    public void DisplayInfo()
-    {
-        Console.WriteLine("Name: " + Name + " Id: " + Id);
-    }
 
-    public void UpdateName(string n)
-    {
-        Name = n;
-    }
+//using System;
 
-    public void UpdateId(int i)
-    {
-        Id = i;
-    }
-}
+//class Student
+//{
+//    public string Name;
+//    public int Id;
 
-class Program
-{
-    static void Main()
-    {
-        Student s1 = new Student("Aniket", 101);
-        Student s2 = new Student("Rahul", 102);
+//    public Student(string name, int id)
+//    {
+//        this.Name = name;
+//        this.Id = id;
+//    }
 
-        s1.DisplayInfo();
-        s2.DisplayInfo();
+//    public void DisplayInfo()
+//    {
+//        Console.WriteLine("Name: " + Name + " Id: " + Id);
+//    }
 
-        s1.UpdateName("Aniket Kumar");
-        s1.UpdateId(201);
+//    public void UpdateName(string n)
+//    {
+//        Name = n;
+//    }
 
-        Console.WriteLine("After Update:");
-        s1.DisplayInfo();
-    }
-}
+//    public void UpdateId(int i)
+//    {
+//        Id = i;
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Student s1 = new Student("Aniket", 101);
+//        Student s2 = new Student("Rahul", 102);
+
+//        s1.DisplayInfo();
+//        s2.DisplayInfo();
+
+//        s1.UpdateName("Aniket Kumar");
+//        s1.UpdateId(201);
+
+//        Console.WriteLine("After Update:");
+//        s1.DisplayInfo();
+//    }
+//}
 
 
 
