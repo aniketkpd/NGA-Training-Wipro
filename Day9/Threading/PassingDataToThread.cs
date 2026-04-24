ff2f2de2660ab87df1ad62762400098049c766fd
Aniket <aniketsingh7855@gmail.com>
2026-04-24 10:43:43 +0530

    day9

---
+++ Day9/Threading/PassingDataToThread.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Day9.Threading
+{
+    internal class PassingDataToThread
+    {
+        static void Message(object? msg)
+        {
+            Console.WriteLine(msg);
+            Thread.Sleep(1000);
+        }
+
+
+
+        public static void Main(string[] args)
+        {
+            //Passing message to thread
+            //Thread t1 = new Thread(Message);
+            //t1.Start("Hello from Thread");
+
+
+            //Lamda Expression
+            //Thread t2 = new Thread(
+            //     () => {
+            //         Thread.Sleep(3000);
+            //         Console.WriteLine("Hello from Thread using Lamda Expression");
+            //     }
+            //    );
+
+            //t2.Start();
+
+
+            Thread t3 = new Thread(
+             (object? mymsg) => {
+                 Thread.Sleep(3000);
+                 Console.WriteLine(mymsg);
+             }
+            );
+
+            t3.Start("Hello From Thread");
+
+            Console.WriteLine("Main is executing");
+        }
+    }
+}
---
+++ Day9/Threading/UseOfTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Day9.Threading
+{
+    class UseOfTask
+    {
+        static void myMethod()
+        {
+            Console.WriteLine("Hello from Task + myMethod");
+        }
+
+        public static void Main(string[] args)
+        {
+            //Task task = Task.Run(() =>
+            //{
+            //    Console.WriteLine("Task is running...");
+            //});
+
+
+            //Task t1 = Task.Run(myMethod);
+
+            //Console.WriteLine("Main thread continues...");
+
+
+
+            //Task that resturn a value
+            //Task<int> → returns int
+            //task.Result → blocks until result is ready
+
+            Task <int> t2 = Task.Run(() =>
+                { 
+                    Thread.Sleep(5000);
+                    return 6;
+                }
+            );
+            Console.WriteLine(t2.Result);
+            Console.WriteLine("Main thread continues...");
+        }
+    }
+}
