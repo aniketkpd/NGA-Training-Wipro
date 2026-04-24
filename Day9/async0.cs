8973b1506123adcef22538fb77d1b095ef61dc8e
Aniket <aniketsingh7855@gmail.com>
2026-04-24 23:43:56 +0530

    day9

---
+++ Day9/async0.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+
+    public class async0
+    {
+        public static async Task Main()
+        {
+            await RunAsynchronous();
+        }
+
+        static async Task RunAsynchronous()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            Task task1 = GetStudentDetailsAsync();
+            Task task2 = GetMarksAsync();
+            Task task3 = GetAttendanceAsync();
+
+            await Task.WhenAll(task1, task2, task3);
+
+            sw.Stop();
+            Console.WriteLine($"Total Time (Async): {sw.ElapsedMilliseconds} ms");
+        }
+
+        static async Task GetStudentDetailsAsync()
+        {
+            await Task.Delay(2000);
+            Console.WriteLine("Student Details Loaded");
+        }
+
+        static async Task GetMarksAsync()
+        {
+            await Task.Delay(2000);
+            Console.WriteLine("Marks Loaded");
+        }
+
+        static async Task GetAttendanceAsync()
+        {
+            await Task.Delay(2000);
+            Console.WriteLine("Attendance Loaded");
+        }
+    }
+}
