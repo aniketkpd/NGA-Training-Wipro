34e09c69dde01bd1a8fd493dd488066d296b0bf1
Aniket <aniketsingh7855@gmail.com>
2026-04-24 13:19:00 +0530

    day9

---
+++ Day9/UseOfAsync&Await.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Day9
+{
+    class UseOfAsync_Await
+    {
+        //static async Task DoWorkAsync()
+        //{
+        //    Console.WriteLine("Downloading...");
+        //    await Task.Delay(5000); // wait 5 seconds
+        //    Console.WriteLine("Done");
+        //}
+
+
+        //static async Task Main()
+        //{
+        //    Console.WriteLine("Start");
+        //    await DoWorkAsync();
+        //    Console.WriteLine("End");
+        //}
+
+
+        static async Task<int> GetNumberAsync()
+        {
+            Console.WriteLine("Downloading the number...");
+            await Task.Delay(5000);
+            return 42;
+        }
+
+        static async Task Main()
+        {
+            //GetNumberAsync().Result; // This will block the main thread until the result is available
+
+            int result = await GetNumberAsync();
+            Console.WriteLine(result);
+        }
+    }
+}
