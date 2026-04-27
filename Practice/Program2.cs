5aa9cda0fc36c44bfa431984858130db18eef142
Aniket <aniketsingh7855@gmail.com>
2026-04-27 15:05:24 +0530

    practice

---
+++ Practice/Program2.cs
@@ -0,0 +1,39 @@
+﻿//single cast and multicast Delegates
+using System;
+namespace practice
+{
+
+    class Program2
+    {
+        //Delegate here - before main and outside main
+        delegate void mydel();
+
+        public static void method1()
+        {
+            Console.WriteLine("method1");
+        }
+        public static void method2()
+        {
+            Console.WriteLine("method2");
+        }
+        public static void method3()
+        {
+            Console.WriteLine("method3");
+        }
+
+
+        public static void Main()
+        {
+            //Single cast
+            // mydel d = method1;
+            // d();
+
+            //multicast
+            mydel d = method1;
+            d += method2;
+            d += method3;
+            d();
+
+        }
+    }
+}
---
+++ Practice/Program3.cs
@@ -0,0 +1,39 @@
+﻿//events
+using System;
+namespace
+{
+    class Program3
+    {
+        //Delegate here - before main and outside main
+        delegate void mydel();
+
+        static event mydel myevent;
+
+
+
+        static void method1()
+        {
+            Console.WriteLine("method1");
+        }
+        static void method2()
+        {
+            Console.WriteLine("method2");
+        }
+        static void method3()
+        {
+            Console.WriteLine("method3");
+        }
+
+
+        static void Main()
+        {
+            myevent += method1;
+            myevent += method2;
+            myevent += method3;
+
+            myevent?.Invoke();
+
+        }
+    }
+
+}
