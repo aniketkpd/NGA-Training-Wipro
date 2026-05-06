237795899bf9ca18f6901aa3e396a30b5a75c25c
Aniket <aniketsingh7855@gmail.com>
2026-05-07 00:00:19 +0530

    practicing razor pages

---
+++ Practice/Razor Pages Apps/WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        [BindProperty]
+        public string? Name { get; set; } = "Aniket";
+        
+        [BindProperty]
+        public string ? Email { get; set; }
+
+        public string? Message { get; set; }
+
+
+        // This method is called when the page is accessed via a GET request/ when user visits the page for the first time
+        public void OnGet()
+        {
+            Email = "myemail@example.com";
+        }
+
+
+        public void OnPost() 
+        {
+            Message = $"Hello {Name}, your email is {Email}";
+        }
+    }
+}
