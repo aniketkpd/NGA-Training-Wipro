1e2f1ddf8a2d551c320675f1f3d0ab32f01e23e7
Aniket <aniketsingh7855@gmail.com>
2026-05-06 16:22:51 +0530

    day 18

---
+++ Day18/CourseRegistrationApp/CourseRegistrationApp/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using CourseRegistrationApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseRegistrationApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Student model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            ViewBag.Message = "Student registered successfully!";
+            return View(model);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Day18/CourseRegistrationApp/CourseRegistrationApp/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseRegistrationApp.Models
+{
+    //This is my model class with fields where validation will be implemented with data annotations 
+    public class Student
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
+
+
+        [Required]
+        public string? Email { get; set; }
+
+
+        [Range(18, 60, ErrorMessage = "Age must be between 18 and 60")]
+        public int Age { get; set; }
+
+    }
+}
