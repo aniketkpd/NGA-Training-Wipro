cb0131d4f092bcf4e478d89025f1205d58af9865
Aniket <aniketsingh7855@gmail.com>
2026-05-07 16:56:49 +0530

    practicing mvc

---
+++ Practice/MVC Apps/WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            StudentModel model = new StudentModel
+            {
+                Name = "John Doe",
+                Age = 0
+            };
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(StudentModel student)
+        {
+            if (ModelState.IsValid)
+            {
+                // Process the valid student data (e.g., save to database)
+                ViewBag.Message = $"Student Name: {student.Name} with age : {student.Age} has been submitted successfully.";
+                return View(student);
+            }
+            else
+            {
+                // If the model is invalid, return the view with validation errors
+                return View(student);
+            }
+        }
+
+
+
+    }
+}
---
+++ Practice/MVC Apps/WebApplication1/WebApplication1/Models/StudentModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class StudentModel
+    {
+        [Required]
+        public string? Name { get; set; }
+
+        [Range(18,60, ErrorMessage = "Age must be between 18 and 60")]
+        public int Age { get; set; }
+    }
+}
