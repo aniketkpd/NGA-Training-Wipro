6e0fccb9323a9a86db9c9e9091fce83b67f54c88
Aniket <aniketsingh7855@gmail.com>
2026-05-08 12:14:12 +0530

    day20

--- Day20/Scenarios/FoodDeliveryApp/FoodDeliveryApp/Program.cs
+++ Day20/Scenarios/FoodDeliveryApp/FoodDeliveryApp/Program.cs
@@ -1,38 +1,46 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
 
 app.MapControllerRoute(
     name: "foodOrder",
     pattern: "order-food",
-    defaults: new { controller = "Restaurant", action = "Menu" });
+    defaults: new
+    {
+        controller = "Restaurant",
+        action = "Menu"
+    });
+
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
 
-app.MapControllers();
 
 app.Run();
-
 
 
 
 // Custom route
 // /Restaurant/Menu or /order-food
 // /Restaurant/Details/5
-
-
-
