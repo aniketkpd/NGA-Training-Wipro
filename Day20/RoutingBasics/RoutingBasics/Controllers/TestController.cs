8fee589c14840a6dafcdd96f87274a7dbed72be2
Aniket <aniketsingh7855@gmail.com>
2026-05-08 10:53:30 +0530

    day20

---
+++ Day20/RoutingBasics/RoutingBasics/Controllers/TestController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+namespace RoutingBasics.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult greet()
+        {
+
+            //This can send message directly to the browswer without the need for a view.
+            return Content("Hello Cosmos!");
+        }
+
+
+
+        public IActionResult greetwithname(string name)
+        {
+
+            //Search here https://localhost:7264/Test/greetwithname?name=aniketkpd
+            return Content($"Hello {name}!");
+        }
+
+
+
+        //This is attribute routing. We can specify the route directly on the action method.
+        //Search here https://localhost:7264/SayHello
+        [Route("SayHello")]
+        public IActionResult SayHello()
+        {
+            return Content("Hello from attribute routing!");
+        }
+
+
+
+        // Attribute Route with Parameter
+        //Search here https://localhost:7264/Welcome/John
+        [Route("Welcome/{name}")]
+        public IActionResult Welcome(string name)
+        {
+            return Content($"Welcome, {name}!");
+        }
+
+
+
+        //Routing Constraint - int
+        [Route("Product/{id:int}")]
+        public IActionResult ShowProduct(int id)
+        {
+            return Content($"Product ID: {id}");
+        }
+
+
+    }
+}
---
+++ Day20/RoutingBasics/RoutingBasics/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RoutingBasics.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
