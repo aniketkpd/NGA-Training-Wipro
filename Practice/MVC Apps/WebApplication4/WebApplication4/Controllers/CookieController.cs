085ffa3742bd508f71f4f4dc07008d4df6b3c550
Aniket <aniketsingh7855@gmail.com>
2026-05-09 21:59:27 +0530

    cookies

---
+++ Practice/MVC Apps/WebApplication4/WebApplication4/Controllers/CookieController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CookieController : Controller
+    {
+        public IActionResult SetCookie()
+        {
+            CookieOptions options = new CookieOptions();
+
+            // set expiration time for the cookie
+            options.Expires = DateTime.Now.AddMinutes(30);
+
+            //set the cookie
+            Response.Cookies.Append("Username", "Aniket", options);
+
+            return Content("Cookie Created");
+        }
+
+        public IActionResult GetCookie()
+        {
+            //get the cookie value
+            string? value = Request.Cookies["Username"];
+
+            return Content("Cookie Value = " + value);
+        }
+
+        public IActionResult DeleteCookie()
+        {
+            //delete the cookie
+            Response.Cookies.Delete("Username");
+
+            return Content("Cookie Deleted");
+        }
+    }
+}
