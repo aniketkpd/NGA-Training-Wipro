3075f5ae36ee707b027850bbe97c86587db60239
Aniket <aniketsingh7855@gmail.com>
2026-05-18 21:51:31 +0530

    day28

---
+++ Day28/ScenarioOnAjax/WebApplication2/WebApplication2/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+
+        [HttpGet]
+        public JsonResult GetStudentById(int id)
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student { Id = 1, Name = "Aniket", Course = "BCA", Age = 21 },
+                new Student { Id = 2, Name = "Rahul", Course = "MCA", Age = 23 },
+                new Student { Id = 3, Name = "Priya", Course = "B.Tech", Age = 22 }
+            };
+
+            var student = students.FirstOrDefault(x => x.Id == id);
+
+            if (student == null)
+            {
+                return Json(null);
+            }
+
+            return Json(student);
+        }
+    }
+}
