e005cf143a88e62237c69f5c6d1ec6b0f80f8e43
Aniket <aniketsingh7855@gmail.com>
2026-05-26 21:20:25 +0530

    Day35

---
+++ Day35/First_Publish_On_Azure_Demo/First_Publish_On_Azure_Demo/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace First_Publish_On_Azure_Demo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
