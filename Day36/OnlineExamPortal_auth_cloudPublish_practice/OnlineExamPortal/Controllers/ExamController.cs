05c760e6e920fd88c1bb58e76a733f363ecba438
Aniket <aniketsingh7855@gmail.com>
2026-05-31 00:25:45 +0530

    Day36

---
+++ Day36/OnlineExamPortal_auth_cloudPublish_practice/OnlineExamPortal/Controllers/ExamController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineExamPortal.Filters;
+
+namespace OnlineExamPortal.Controllers
+{
+
+
+    [CustomLogFilter]
+    public class ExamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Exam");
+        }
+    }
+}
